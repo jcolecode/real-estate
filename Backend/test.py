a38ad88c2e4e043e64e630ea0a9ba7063e29c35f
jcolecode <jacobcole152@gmail.com>
2023-10-07 12:27:11 -0500

    Updates

--- Backend/test.py
+++ Backend/test.py
@@ -2,9 +2,19 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
 import time
+import openpyxl
 
+# Variables 
 lara_url = "https://cofs.lara.state.mi.us/SearchApi/Search/Search"
 owner1 = "G.M.K., L.L.C."
+owner2 = "AB11 CORPORATION"
+
+# Initialize excel
+workbook = openpyxl.Workbook()
+worksheet = workbook.active
+
+# Create a set to store unique names
+unique_names = set()
 
 # LARA
 def startLara(url, owner):
@@ -14,27 +24,47 @@ def startLara(url, owner):
 
     # Open url in chrome
     driver.get(url)
-
     time.sleep(3)
 
     driver.find_element(By.ID, "txtEntityName").send_keys(owner)
-
     time.sleep(3)
 
     driver.find_element(By.ID, "SearchSubmit").click()
-
     time.sleep(3)
 
     # TODO IF THERE ARE MULPITPLE OPTIONS FOR LLC CHECK THE ADDRESS FROM LANDVISON
-    driver.find_element(By.XPATH, '//a[@href="' + "https://cofs.lara.state.mi.us/CorpWeb/CorpSearch/CorpSummary.aspx?token=nBxILn58HwVtv4JMRDwTm1cWblopjmzIgq3FCQzRMH7Z0mRAdeXC1B5BhZoRn0rX3c+Fp+Gbnx2wRL9zMGX1/sqhAMEqWytza0G4dL88Kii9r8OTSljKNqvdnbvImpekDgIM4oOuLyKYNJ5StGnZPZoKheK8QThGI0ug80NLPCzCtiv7EGbX4jSx3ouV2Zjy5Qm+v6OzxOMA/HplVdLzxkw3cZwjP27lAx5LCRGE2ZFSc0ygP37CQCJ5pQFzVU7uCL9rpuRfy5+xp3WO0wsgZTfDSJRqmg+Ff1cDuBTDo/tWBmbGZ5EtFbd7cBsUl4qwkAVNmUcdtgRQ7xoNebsN5AeSWszabZBf" + '"]').click()
+    driver.find_element(By.XPATH, f"//a[contains(text(), '{owner}')]").click()
     time.sleep(3)
 
     resident_element = driver.find_element(By.ID, "MainContent_lblResidentAgentName")
     resident = resident_element.text
-    return resident
+    names = resident.split()
 
-    # TODO GET FIRST NAME FROM THIS RESIDENT
-    
-    # TODO GET LAST NAME FROM THIS RESIDENT
+    if len(names) >= 2:
+        first_name = names[0]
+        last_name = names[1]
 
+        # Check if the name is not already in the set
+        if (first_name, last_name) not in unique_names:
+            # Add the name to the set of unique names
+            unique_names.add((first_name, last_name))
+
+            # Write names to the Excel file
+            row = len(unique_names)  # Get the row number for this name
+            worksheet.cell(row=row, column=1, value=first_name)
+            worksheet.cell(row=row, column=2, value=last_name)
+
+            # Save the workbook and close the driver
+            workbook.save('../excel/output_test.xlsx')
+            driver.quit()
+
+        # Return the first and last name (optional)
+        return first_name
+        return last_name
+    else: 
+        print("The text does not contain two words.")
+
+
+# Method calls
 startLara(lara_url, owner1)
+startLara(lara_url, owner2)
